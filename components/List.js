66f96db5917bb143393a7e91e73f42accf7c3085
sum127 <tnaud940127@naver.com>
2021-04-20 10:17:02 +0900

    final

--- components/List.js
+++ components/List.js
@@ -1,9 +1,9 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Text} from 'react-native';
+import { Text } from 'react-native';
 
 import { LISTDATA } from '../share/list'
 
-import {  Card } from 'react-native-elements'
+import { Card } from 'react-native-elements'
 import { ScrollView } from 'react-native-gesture-handler'
 import api from '../api/list'
 // 함수의 리턴 값이 JSX.Element면
@@ -11,67 +11,67 @@ import api from '../api/list'
 
 // JSX를 쓸려면 import React from 'react';
 // Navigator로 화면을 이동할 때 컴포넌트 속성으로 전달됨
-const List = ({navigation}) => {
+const List = ({ navigation }) => {
 
 
-    // 백엔드에 API요청후 응답받은 데이터를 화면에 다시렌터딩
-    // state 처리
-    const [list, setList] = useState([]);
-    
-    
-    // 특정조건에만 함수가 생성되도록 하는것이 usecallback
-    // , [] 함수가 재생성될 조건
-    // ex [data]  <- data라는 객체변수가 생성되거나 바뀔때 함수가 재생성
-    // [] 안에가 빈값이면 컴포넌트가 처음 마운트 됐을때만 함수생성
-    // []빼면 항상생성됨
-    const getList =useCallback(async () => {
-      // 해당컴포넌트에 async 가있어야 await사용가능
-      const result = await api.list();
-      console.log(result.data);
-      setList(result.data);
-    }, [])
-  
-    // useEffect는 특정조건에 맞게 실행되는 함수를 정의
-    // , [] 컴포넌트가 처음 마운트 됐을때만 함수생성
-    // ex [data]  <- data라는 객체변수가 생성되거나 바뀔때 함수가 실행됨
-    // useEffect에는 async 정의가 안돼서 usecallback과 함께사용(event hook)
-    // []빼면 항상실행됨
-    // useEffect(()=>{
-    //   getList();
-    // }, [])
-  
-    // 텝화면 이동시 새로 갱신하는법
-    
-    useEffect(()=>{
-      // navigation 이벤트 리스너를 생성
-      // 반환값이 이벤트 리스너 해제 함수
-      const  unsubscribe = navigation.addListener(
-        'focus',
-        () => {
-          console.log('focus')
-          getList();
-        }
-      )
-      // clean-up function(객체소멸) (업데이트를 위한 이전함수 소멸)
-      // useEffect(()=>{ ... return 함수명 },[])
-  
-      // component가 unmount 되는 시점에 clean up 함수가 실행됨
-      return unsubscribe;
-    },[navigation])
-  
+  // 백엔드에 API요청후 응답받은 데이터를 화면에 다시렌터딩
+  // state 처리
+  const [list, setList] = useState([]);
+
+
+  // 특정조건에만 함수가 생성되도록 하는것이 usecallback
+  // , [] 함수가 재생성될 조건
+  // ex [data]  <- data라는 객체변수가 생성되거나 바뀔때 함수가 재생성
+  // [] 안에가 빈값이면 컴포넌트가 처음 마운트 됐을때만 함수생성
+  // []빼면 항상생성됨
+  const getList = useCallback(async () => {
+    // 해당컴포넌트에 async 가있어야 await사용가능
+    const result = await api.list();
+    console.log(result.data);
+    setList(result.data);
+  }, [])
+
+  // useEffect는 특정조건에 맞게 실행되는 함수를 정의
+  // , [] 컴포넌트가 처음 마운트 됐을때만 함수생성
+  // ex [data]  <- data라는 객체변수가 생성되거나 바뀔때 함수가 실행됨
+  // useEffect에는 async 정의가 안돼서 usecallback과 함께사용(event hook)
+  // []빼면 항상실행됨
+  // useEffect(()=>{
+  //   getList();
+  // }, [])
+
+  // 텝화면 이동시 새로 갱신하는법
+
+  useEffect(() => {
+    // navigation 이벤트 리스너를 생성
+    // 반환값이 이벤트 리스너 해제 함수
+    const unsubscribe = navigation.addListener(
+      'focus',
+      () => {
+        console.log('focus')
+        getList();
+      }
+    )
+    // clean-up function(객체소멸) (업데이트를 위한 이전함수 소멸)
+    // useEffect(()=>{ ... return 함수명 },[])
+
+    // component가 unmount 되는 시점에 clean up 함수가 실행됨
+    return unsubscribe;
+  }, [navigation])
+
   return (
-     <ScrollView>
-        {
-          list.map((item, i) => (
-           <Card key={i} >
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Image source={{uri: item.image}} onPress={()=>{navigation.navigate("Details", {id: item.id})}}></Card.Image>
-                <Text style={{textAlign:'right', fontSize:20, marginTop:10}}> 190,000,000원</Text>
-            </Card>
-          ))
-        }
-      </ScrollView>
-    
+    <ScrollView>
+      {
+        list.map((item, i) => (
+          <Card key={i} >
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Image source={{ uri: item.image }} onPress={() => { navigation.navigate("Details", { id: item.id }) }}></Card.Image>
+            <Text style={{ textAlign: 'right', fontSize: 20, marginTop: 10 }}>100,000원</Text>
+          </Card>
+        ))
+      }
+    </ScrollView>
+
   )
 }
 export default List;
--- components/MainContainer.js
+++ components/MainContainer.js
@@ -1,4 +1,4 @@
-import React,{ useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -13,7 +13,7 @@ import Tasks from './Tasks'
 
 import { Alert } from 'react-native'
 
-import { useDispatch, useSelector  } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const TaskStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -21,72 +21,73 @@ const ListStack = createStackNavigator();
 const HomeStack = createStackNavigator();
 
 const HomeStackScreen = () => {
-    return (
-      <HomeStack.Navigator>
-        <HomeStack.Screen name="Home" component={Home} options={{title:"Home", headerTitleAlign:"center"
-             
-           }} />
-           <HomeStack.Screen name="Details" component={Details} options={{title:"Details", headerTitleAlign:"center"}}  />
-      </HomeStack.Navigator>
-    )
-  }
-  
-  const ListStackScreen = () => {
-    return (
-      <ListStack.Navigator>
-        <ListStack.Screen name="List" component={List} options={{title:"List", headerTitleAlign:"center"}} />
-        <HomeStack.Screen name="Details" component={Details} options={{title:"Details", headerTitleAlign:"center"}}  />
-      </ListStack.Navigator>
-    )
-  }
-  const TaskStackScreen = () => {
-    return (
-      <TaskStack.Navigator>
-        <TaskStack.Screen name="Tasks" component={Tasks} options={{title:"Tasks", headerTitleAlign:"center"}} />
-      </TaskStack.Navigator>
-    )
-  }
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} options={{
+        title: "Home", headerTitleAlign: "center"
 
-  const tabBarOptions= {
-    activeTintColor: 'white',
-    inactiveTintColor: 'gray',
-    activeBackgroundColor:'black',
-    labelStyle:{
-      fontSize:20,
-      marginBottom : 10
-    }
-  }
-  export default function Main(){
-    const dispatch = useDispatch();
-    useEffect(()=>{
-      console.log("-- main is mounted--")
-      // back-end에서 tasks 데이터를 가져오고, global state를 갱신
-      dispatch({type:"FETCH_TASKS"})
-    }, [])
+      }} />
+      <HomeStack.Screen name="Details" component={Details} options={{ title: "Details", headerTitleAlign: "center" }} />
+    </HomeStack.Navigator>
+  )
+}
 
-    const alert = useSelector(state => state.alert)
+const ListStackScreen = () => {
+  return (
+    <ListStack.Navigator>
+      <ListStack.Screen name="Tour" component={List} options={{ title: "List", headerTitleAlign: "center" }} />
+      <HomeStack.Screen name="Details" component={Details} options={{ title: "Details", headerTitleAlign: "center" }} />
+    </ListStack.Navigator>
+  )
+}
+const TaskStackScreen = () => {
+  return (
+    <TaskStack.Navigator>
+      <TaskStack.Screen name="Booking" component={Tasks} options={{ title: "Booking", headerTitleAlign: "center" }} />
+    </TaskStack.Navigator>
+  )
+}
+
+const tabBarOptions = {
+  activeTintColor: 'white',
+  inactiveTintColor: 'gray',
+  activeBackgroundColor: 'black',
+  labelStyle: {
+    fontSize: 20,
+    marginBottom: 10
+  }
+}
+export default function Main() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log("-- main is mounted--")
+    // back-end에서 tasks 데이터를 가져오고, global state를 갱신
+    dispatch({ type: "FETCH_TASKS" })
+  }, [])
+
+  const alert = useSelector(state => state.alert)
   console.log('--alert--')
   console.log(alert)
 
-  if(alert.isShow) {
+  if (alert.isShow) {
     Alert.alert(
       "Errors",
       alert.msg,
       [
-        { text: "OK", onPress: () => dispatch({type:"CLOSE_ALERT"}) }
+        { text: "OK", onPress: () => dispatch({ type: "CLOSE_ALERT" }) }
       ],
       { cancelable: false }
     );
-  } 
-    return(
-        <SafeAreaProvider>
-        <NavigationContainer>
-          <Tab.Navigator  tabBarOptions={tabBarOptions}>
-            <Tab.Screen name="Home" component={HomeStackScreen} />
-            <Tab.Screen name="List" component={ListStackScreen} />
-            <Tab.Screen name="Tasks" component={TaskStackScreen} />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    )
   }
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator tabBarOptions={tabBarOptions}>
+          <Tab.Screen name="Home" component={HomeStackScreen} />
+          <Tab.Screen name="Tour" component={ListStackScreen} />
+          <Tab.Screen name="Booking" component={TaskStackScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
+}
